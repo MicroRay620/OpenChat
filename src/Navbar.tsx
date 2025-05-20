68f82c1a40db094a0510b8e419907049c11c2aba
MicroRay620 <microray620@gmail.com>
2025-05-20 11:32:01 -0400

    created and updated. Created Navbar and modified App.css to have tailwindcss

---
+++ src/Navbar.tsx
@@ -0,0 +1,9 @@
+function Navbar() {
+  return (
+    <header itemID="oc-app-main-header">
+      <nav itemID="oc-app-navbar">
+        <ul itemID="oc-app-nav-list" className="navbar"></ul>
+      </nav>
+    </header>
+  );
+}
