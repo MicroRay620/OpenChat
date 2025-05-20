c39aa24770e04c83c2a64a9e587805e94651b002
MicroRay620 <microray620@gmail.com>
2025-05-20 13:25:25 -0400

    modified

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -2,8 +2,12 @@ function Navbar() {
   return (
     <header itemID="oc-app-main-header">
       <nav itemID="oc-app-navbar">
-        <ul itemID="oc-app-nav-list" className="navbar"></ul>
+        <ul itemID="oc-app-nav-list" className="navbar">
+          <li itemID="oc-app-nav-item"></li>
+        </ul>
       </nav>
     </header>
   );
 }
+
+export default Navbar;
