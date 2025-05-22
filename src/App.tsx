c73a3edb9eee8130254c286fcc409f288897ad59
MicroRay620 <microray620@gmail.com>
2025-05-22 13:19:03 -0400

    deleted pregenerated code

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
 
 function App() {
-    return();
 }
 
 export default App;
