154837d09d6b852ac0543a8c7172de31fc9f13e7
MicroRay620 <microray620@gmail.com>
2025-05-27 23:38:17 -0400

    created

---
+++ Login.tsx
@@ -0,0 +1,25 @@
+import "./Login.css";
+import React from "react";
+import { invoke } from "@tauri-apps/api/core";
+import { useState } from "react";
+function Login() {
+    const [username, setState]: string = useState("username");
+    const [email, setState]: string = useState("email");
+    const [password, setState]: string = useState("password");
+    return (
+        <div itemID={"user-login-container"}>
+            <h3 itemID={"login-heading"} className={""}></h3>
+            <form itemID={"user-login-form"}>
+                <label htmlFor={"username"} className={"cls-user-label"}>Username</label>
+                <input type={"text"} itemID={"username"} className={"cls-user-input"} value={username}/>
+                <label htmlFor={"email"} className={"cls-user-label"}>Email</label>
+                <input type={"email"} itemID={"email"} className={"cls-user-input"} value={email}/>
+                <label htmlFor={"password"} className={"cls-user-label"}>Password</label>
+                <input type={"password"} itemID={"password"} className={"cls-user-input"} value={password}/>
+                <button type={"submit"} itemID={"login-button"} className={"cls-user-button"}>Register</button>
+            </form>
+            <span itemID={"login-option"}>Already have an account? <a hrefLang="#">Login</a></span>
+        </div>
+    );
+}
+export default  Login();
