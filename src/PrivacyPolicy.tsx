b9199702cd6548dcfb75c179b935cae610fd3d81
ruby_rose <46199301+MicroRay620@users.noreply.github.com>
2025-06-20 20:38:59 -0400

    Created

---
+++ src/PrivacyPolicy.tsx
@@ -0,0 +1,28 @@
+export default function PrivacyPolicy() {
+    return (
+        <>
+            <div id={"cl-main-privacy-policy-container"}>
+                <div id={"cl-main-privacy-policy-header-container"}>
+                    <h1 className={"text-4xl"}>Privacy Policy</h1>
+                </div>
+                <div id={"cl-main-privacy-policy-text-container"}>
+                    <section id="cl-main-privacy-policy-section-one">
+                        <h2 className={"text-3xl"}>User Privacy</h2>
+                        <article id={"cl-main-privacy-polict-user-privacy-article-container"}>
+                            <span id={"cl-main-privacy-polict-user-privacy-text"} className={"my-4 px-50 block text-left"}>
+                                <ul className={"list-decimal pl-10"}>
+                                    <li>The data collected and stored for CypherLynx that is stored via cookies. This data is including but not limited to the Communities you're in, the Communities you're banned from, your username, you password, your email address, a private custom url link to your avatar in your profile picture, your IP address, and more.</li>
+                                    <li>Your data will <strong><em>NOT</em></strong> be sold to third-parties unless requested for things such as audits, warrants, checking illegal activity, and testing if an account has been hacked.</li>
+                                    <li>All data that is stored can be accessed easily by requesting a copy for your data and it will show the complete and thorough list of all of your data we collect.</li>
+                                    <li>You may request all non-vital data (vital data is your username, password, and email address) to be removed at anytime via an email or support ticket</li>
+                                    <li>You may not see message data or having that data be removed. <br /><small>This is for making sure your data can searched for illegal activity. Any extensions to remove this restriction will result in an automatic removal from the download page and a strike on the creator's account</small></li>
+                                    <li>If discovered that you are doing serious illegal activity (such as viewing and/or distributing child porn, encouraging the spread of hate speech and/or violence, or other criminal activity of the like), your account will be IP banned from using CypherLynx. Other activity will result in a temporary suspension from CypherLynx for a limited time determined by the severity of the crime</li>
+                                </ul>
+                            </span>
+                        </article>
+                    </section>
+                </div>
+            </div>
+        </>
+    )
+}
---
+++ src/SectionBreaks.tsx
@@ -0,0 +1,19 @@
+export function SectionBreak() {
+    return (
+        <>
+            <span id={"cl-main-section-break"}>
+                &mdash;&mdash;&mdash;
+            </span>
+        </>
+    )
+}
+
+export function ContainerBreak() {
+    return (
+        <>
+            <span id={"cl-main-container-break"}>
+                &mdash;&mdash;&mdash;&mdash;&mdash;&mdash;
+            </span>
+        </>
+    )
+}
