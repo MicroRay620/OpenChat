e1476e16bcc08b2350efe387bdbdb2f0f0c89688
ruby_rose <46199301+MicroRay620@users.noreply.github.com>
2025-06-20 20:40:18 -0400

    Added ToS and Privacy Policy renders

--- src/App.tsx
+++ src/App.tsx
@@ -6,21 +6,28 @@ import Download from "./Download";
 import DocsHome from "./Doc-home";
 import Login from "./Login";
 import Home from "./Home";
+import PrivacyPolicy from "./PrivacyPolicy";
+import ToS from "./ToS";
 
 function App() {
     return (
-        <BrowserRouter>
-            <div className="text-center dark:bg-gray-800 dark:text-white">
-                <Navbar />
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/download" element={<Download />} />
-                    <Route path="/docs" element={<DocsHome />} />
-                    <Route path="/login" element={<Login />} />
-                </Routes>
-                <Footer />
-            </div>
-        </BrowserRouter>
+        <>
+            <BrowserRouter>
+                <div className="text-center dark:bg-gray-800 dark:text-white min-h-screen flex flex-col">
+                    <Navbar />
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/download" element={<Download />} />
+                        <Route path="/docs" element={<DocsHome />} />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+                        <Route path="/tos" element={<ToS />} />
+                    </Routes>
+                    <br/>
+                    <Footer />
+                </div>
+            </BrowserRouter>
+        </>
     );
 }
 
