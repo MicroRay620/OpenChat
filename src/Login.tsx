04b8552c84d7278d54b1a8c318809516a7a23e58
ruby_rose <46199301+MicroRay620@users.noreply.github.com>
2025-06-20 20:40:44 -0400

    Expanded and stylized the login screen

--- src/Login.tsx
+++ src/Login.tsx
@@ -13,30 +13,26 @@ export default function Login() {
 
     return (
         <>
-            <br/>
             <div id={"user-login-container"}>
+                <br/>
                 <h3 id={"login-heading"}>Register</h3>
-                {/*
-                * Make into a table form
-                */}
-                <form id={"user-login-form"} onSubmit={handleSubmit}>
-                    <label htmlFor={"username"} className={"cls-user-label"}>Username</label> &nbsp;
-                    <input type={"text"} id={"username"} className={"cls-user-input"} value={username} onChange={(e) => setUsername(e.target.value)}/>
-                    <br/>
-                    {/*
-                        * The <br/> are temporary, they will be changed for divs 
-                    */}
-                    <label htmlFor={"email"} className={"cls-user-label"}>Email</label> &nbsp;
-                    <input type={"email"} id={"email"} className={"cls-user-input"} value={email} onChange={(e) => setEmail(e.target.value)}/>
-                    <br/>
-                    <label htmlFor={"password"} className={"cls-user-label"}>Password</label> &nbsp;
-                    <input type={"password"} id={"password"} className={"cls-user-input"} value={password} onChange={(e) => setPassword(e.target.value)}/>
-                    <br/>
-                    <button type={"submit"} id={"login-button"} className={"cls-user-button hover:text-blue-600"}>Register</button>
+                <form id={"cl-user-login-form"} onSubmit={handleSubmit}>
+                    <div id={"cl-user-login-username-container"}>
+                        <label htmlFor={"username"} className={"cl-user-label"}>Username</label> &nbsp;
+                        <input type={"text"} id={"username"} className={"cl-user-input border border-white bg-transparent text-white px-3 py-2 rounded w-full mt-1"} value={username} onChange={(e) => setUsername(e.target.value)}/>
+                    </div>
+                    <div id={"cl-user-login-email-container"}>
+                        <label htmlFor={"email"} className={"cl-user-label"}>Email</label> &nbsp;
+                        <input type={"email"} id={"email"} className={"cl-user-input border border-white bg-transparent text-white px-3 py-2 rounded w-full mt-1"} value={email} onChange={(e) => setEmail(e.target.value)}/>
+                    </div>
+                    <div id={"cl-user-login-password-container"}>
+                        <label htmlFor={"password"} className={"cl-user-label"}>Password</label> &nbsp;
+                        <input type={"password"} id={"password"} className={"cl-user-input border border-white bg-transparent text-white px-3 py-2 rounded w-full mt-1"} value={password} onChange={(e) => setPassword(e.target.value)}/>
+                    </div>
+                    <button type={"submit"} id={"login-button"} className={"cl-user-button hover:text-blue-600"}>Register</button>
                 </form>
                 <span id={"login-option"}>Already have an account? <a href={"#"}>Login</a></span>
             </div>
-        </>
-       
+        </> 
     );
 }
