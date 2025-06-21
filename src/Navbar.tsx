985f72b4092b41887764c68023b6cf4d036f6222
ruby_rose <46199301+MicroRay620@users.noreply.github.com>
2025-06-20 20:43:16 -0400

    changed the apps to cl-main for clarity of the page. changed formatting to match other files, and added line breaks at the bottom

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -3,62 +3,66 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <header id="app-main-header" className="app-navbar-header">
-      <nav id="app-main-navbar">
-        <ul id="app-main-nav-list" className="app-navbar">
-          <li className="group relative">
-            <Link to="/" id="app-main-home" className="nav-link">
-              <span id="app-nav-span-home" className="nav-span hover:text-blue-600">
-                <BsHouse id="app-home-icon" />
-              </span>
-              <div className="absolute z-10 opacity-0 group-hover:opacity-100 transition-opacity duration-200 mt-2 left-1/2 transform -translate-x-1/2 w-max">
-                <div className="bg-gray-800 text-white text-xs rounded py-1 px-2 whitespace-nowrap">
-                  Home
+    <>
+      <header id={ "cl-main-header" } className={ "cl-navbar-header" }>
+        <nav id={ "cl-main-navbar" }>
+          <ul id={ "cl-main-nav-list" } className={ "cl-navbar justify-center text-3xl flex list-none p-0 m-0 gap-1" }>
+            <li className={ "group relative" }>
+              <Link to={ "/" } id={ "cl-main-home" } className={ "nav-link" }>
+                <span id={ "cl-nav-span-home" } className={ "nav-span hover:text-blue-400" }>
+                  <BsHouse id={ "cl-home-icon" } />
+                </span>
+                <div className={ "absolute z-10 opacity-0 group-hover:opacity-100 transition-opacity duration-200 mt-2 left-1/2 transform -translate-x-1/2 w-max" }>
+                  <div className={ "bg-gray-800 text-white text-xs rounded py-1 px-2 whitespace-nowrap" }>
+                    Home
+                  </div>
                 </div>
-              </div>
-            </Link>
-          </li>
+              </Link>
+            </li>
 
-          <li className="group relative">
-            <Link to="/download" id="app-main-download" className="nav-link">
-              <span id="app-nav-span-download" className="nav-span hover:text-blue-600">
-                <BsBoxArrowInDown />
-              </span>
-              <div className="absolute z-10 opacity-0 group-hover:opacity-100 transition-opacity duration-200 mt-2 left-1/2 transform -translate-x-1/2 w-max">
-                <div className="bg-gray-800 text-white text-xs rounded py-1 px-2 whitespace-nowrap">
-                  Downloads
+            <li className={ "group relative" }>
+              <Link to={ "/download" } id={ "cl-main-download" } className={ "nav-link" }>
+                <span id={ "cl-nav-span-download" } className={ "nav-span hover:text-blue-400" }>
+                  <BsBoxArrowInDown />
+                </span>
+                <div className={ "absolute z-10 opacity-0 group-hover:opacity-100 transition-opacity duration-200 mt-2 left-1/2 transform -translate-x-1/2 w-max" }>
+                  <div className={ "bg-gray-800 text-white text-xs rounded py-1 px-2 whitespace-nowrap" }>
+                    Downloads
+                  </div>
                 </div>
-              </div>
-            </Link>
-          </li>
+              </Link>
+            </li>
 
-          <li className="group relative">
-            <Link to="/docs" id="app-main-docs" className="nav-link">
-              <span id="app-nav-span-docs" className="nav-span hover:text-blue-600">
-                <BsBook />
-              </span>
-              <div className="absolute z-10 opacity-0 group-hover:opacity-100 transition-opacity duration-200 mt-2 left-1/2 transform -translate-x-1/2 w-max">
-                <div className="bg-gray-800 text-white text-xs rounded py-1 px-2 whitespace-nowrap">
-                  Documentation
+            <li className={ "group relative" }>
+              <Link to={ "/docs" } id={ "cl-main-docs" } className={ "nav-link" }>
+                <span id={ "cl-nav-span-docs" } className={ "nav-span hover:text-blue-400" }>
+                  <BsBook />
+                </span>
+                <div className={ "absolute z-10 opacity-0 group-hover:opacity-100 transition-opacity duration-200 mt-2 left-1/2 transform -translate-x-1/2 w-max" }>
+                  <div className={ "bg-gray-800 text-white text-xs rounded py-1 px-2 whitespace-nowrap" }>
+                    Documentation
+                  </div>
                 </div>
-              </div>
-            </Link>
-          </li>
+              </Link>
+            </li>
 
-          <li className="group relative">
-            <Link to="/login" id="app-main-login" className="nav-link">
-              <span id="app-nav-span-login" className="nav-span hover:text-blue-600">
-                <BsPerson />
-              </span>
-              <div className="absolute z-10 opacity-0 group-hover:opacity-100 transition-opacity duration-200 mt-2 left-1/2 transform -translate-x-1/2 w-max">
-                <div className="bg-gray-800 text-white text-xs rounded py-1 px-2 whitespace-nowrap">
-                  Login
+            <li className={ "group relative" }>
+              <Link to={ "/login" } id={ "cl-main-login" } className={ "nav-link" }>
+                <span id={ "cl-nav-span-login" } className={ "nav-span hover:text-blue-400" }>
+                  <BsPerson />
+                </span>
+                <div className={ "absolute z-10 opacity-0 group-hover:opacity-100 transition-opacity duration-200 mt-2 left-1/2 transform -translate-x-1/2 w-max" }>
+                  <div className={ "bg-gray-800 text-white text-xs rounded py-1 px-2 whitespace-nowrap" }>
+                    Login
+                  </div>
                 </div>
-              </div>
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <br/>
+      <br/>
+    </>
   );
 }
